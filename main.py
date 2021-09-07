613f01b8dcbf9084b05aa5d9444bc9d21de1f341
Danilo Rechi <fixmyprinter@danilorechi.net>
2021-09-07 23:48:19 +0000

    feat(buzzer): add minimal buzzer support

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from rpi_lcd import LCD
 from gpiozero import Button
 from src.main_menu import get_main_menu
-from gpiozero import Button
+from gpiozero import Button, TonalBuzzer
+from gpiozero.tones import Tone
 from time import sleep
 
 main_menu = get_main_menu()
@@ -9,6 +10,7 @@ main_menu.first_item()
 
 lcd = LCD()
 button = Button(26)
+buzzer = TonalBuzzer(19)
 
 while True:
     lines = main_menu.get_data()
@@ -17,4 +19,8 @@ while True:
     
     button.wait_for_press()
     main_menu.next_item()
+
+    buzzer.play(Tone(510.0)) #510, 550, 590 (each button)
+    sleep(0.1)
+    buzzer.stop()
 
