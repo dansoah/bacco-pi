830163b7982d0c250f854d6345cd3383efaf42ef
Danilo Rechi <fixmyprinter@danilorechi.net>
2021-09-07 23:56:22 +0000

    feat(date-time): add date time sub item

--- src/main_menu.py
+++ src/main_menu.py
@@ -1,7 +1,8 @@
 from src.menu.menu import Menu
 from src.menu.menu_item import MenuItem
-
+import datetime
 import socket
+    
 def get_ip_addr():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
@@ -16,6 +17,12 @@ def draw_ip_addr():
 def draw_hello_screen():
     return ["Hello!", "I'm awake :)"]
 
+def draw_current_time():
+    return [
+        datetime.datetime.now().strftime('%Y-%m-%d'),
+        datetime.datetime.now().strftime('%H:%M:%S')
+    ]
+
 def get_main_menu():
     main_menu = Menu("Main")
 
@@ -25,7 +32,11 @@ def get_main_menu():
     ip_addr = MenuItem()
     ip_addr.set_handler(draw_ip_addr)
 
+    date_time = MenuItem()
+    date_time.set_handler(draw_current_time)
+
     main_menu.add_item(hello_screen)
     main_menu.add_item(ip_addr)
+    main_menu.add_item(date_time)
     
     return main_menu
